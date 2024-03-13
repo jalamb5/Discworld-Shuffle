15ebe1e94920ae9f1a5360822209254d74b61190
Justin Lamb <justinlamb@me.com>
2024-03-13 15:54:46 -0700

    add messages

--- src/board.jsx
+++ src/board.jsx
@@ -21,6 +21,7 @@ function Board() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [clickedCards, setClickedCards] = useState(() => []);
+  const [message, setMessage] = useState(() => '')
   const [cards, setCards] = useState(() =>
     shuffle(
       novels.value.map((isbn, index) => (
@@ -33,6 +34,10 @@ function Board() {
     setClickedCards((prevClickedCards) => {
       // Check if the card was already clicked
       if (prevClickedCards.includes(key)) {
+        setMessage((prevMessage) => 'Oops! Try Again!')
+        setTimeout(() => {
+          setMessage((prevMessage) => '');
+        }, 3000)
         // Reset the game by reshuffling the cards, setting score to 0, and clearing clicked cards
         setCards((prevCards) => shuffle([...prevCards]));
         setScore(0);
@@ -53,13 +58,20 @@ function Board() {
   // Set highscore
   if (score > highScore) {
     setHighScore(score);
+    setMessage((prevMessage) => 'New high score!')
+    setTimeout(() => {
+      setMessage((prevMessage) => '');
+    }, 700)
   }
 
   return (
     <div>
-      <h2>
-        Score: {score} - High Score: {highScore}
-      </h2>
+      <div id="scores">
+        <h2>Score: {score}</h2>
+        <h2 className={message === '' ? "no-message" : "message" }>{message}</h2>
+        <h2>High Score: {highScore}</h2>
+      </div>
+
       <div className="board">{cards}</div>
     </div>
   );
