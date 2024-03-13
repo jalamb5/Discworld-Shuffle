56625f39e852f2cb16ba46c65ea7897c84fa4fd4
Justin Lamb <justinlamb@me.com>
2024-03-13 15:25:46 -0700

    explainer text, css tweaks

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <h1>Discworld Memory Game</h1>
+    <p>How well do you know the Discworld series?</p>
+    <p>Click each novel only once and try to beat your high score!</p>
     <Board />
   </React.StrictMode>,
 )
